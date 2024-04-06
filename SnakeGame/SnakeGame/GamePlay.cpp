df2f80f89a55bbdf11acdf79a653e48f6c95325e
23521054 <23521054@gm.uit.edu.vn>
2024-04-06 22:47:29 +0700

    create the snake
    update

--- SnakeGame/SnakeGame/GamePlay.cpp
+++ SnakeGame/SnakeGame/GamePlay.cpp
@@ -1,6 +1,8 @@
 #include "GamePlay.h"
 #include <raylib.h>
 #include <iostream>
+#include"Snake.h"
+#include<conio.h>
 using namespace std;
 
 void GamePlay::Start()
@@ -9,15 +11,30 @@ void GamePlay::Start()
 
     SetTargetFPS(60);
 
+    Snake snake(GRID_SIZE / 2, GRID_SIZE / 2); 
+
+    char key;
+
     while (!WindowShouldClose()) {
+        if (_kbhit())
+        {
+            key = _getch();
+            snake.SetDirection(key);
+
+        }
+        snake.Move();
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         GamePlay::DrawGrid(GRID_ROWS, GRID_COLS, GRID_SIZE);
 
-        EndDrawing();
-    }
+        
+        snake.Draw(GRID_SIZE);
 
+        
+        EndDrawing();   
+        
+    }
     CloseWindow();
 }
 
--- SnakeGame/SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame/SnakeGame.cpp
@@ -1,10 +1,12 @@
 #include "raylib.h"
 #include "GamePlay.h"
-
+#include<conio.h>
+#include"Snake.h"
 
 int main(void) {
 
     GamePlay x;
     x.Start();
+    
     return 0;
 }
